58b2fe05d1c18b5525b6e0f7013d9d4c7aba7364
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-17 05:23:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,14 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  array = seaon_hash[season]
+  count = 0
+  until count == array.length
+  hash = array[count]
+  if hash["status"] == "Winner"
+    name = hash["name"].split
+    return name[0]
+  end
+  count += 1
+end
 end
 
 def get_contestant_name(data, occupation)

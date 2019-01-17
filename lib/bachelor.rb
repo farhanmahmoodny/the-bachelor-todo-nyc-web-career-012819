9894d7353fd98e15b0c749cb9ae05a21b3cb77d5
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-17 05:54:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -26,7 +26,7 @@ def count_contestants_by_hometown(data, hometown)
  data.each do |season, array|
    array.each do |contestant|
      if contestant["hometown"] == hometown
-       count ++1
+       count += 1
      end
    end
  end

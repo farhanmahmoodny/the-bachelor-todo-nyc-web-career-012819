12b57676b6d12bc73e15310e8f3ddecc24e35f01
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-17 06:02:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,7 +50,7 @@ def get_average_age_for_season(data, season)
   until count == array.length
   num = array[count]["age"].to_i
     sum += num
-    count +=1
+    count += 1
   end
   sum / array.length
 end
